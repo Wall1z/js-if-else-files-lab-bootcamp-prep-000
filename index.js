eb4adff3599456c203e7aa809e88d10bb21ebdf8
wael alnajar <w_alnajar@yahoo.com>
2017-09-17 12:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var name = "janet"
-var height = 77
+var height = 74
+var message = `${name} is ${height} inches tall`
 
+module.exports = {name, height, message}
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
 console.log(height+1)
+console.log(message)
